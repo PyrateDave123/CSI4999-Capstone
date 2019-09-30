c48099c09de0a4d432f2ff8d4a0fc842c429d1bd
David <michondavj@gmail.com>
2019-09-30 16:37:53 -0400

    Camera integration completed into tab3 which has been turned into the Camera/Gallery tab

--- capstone/src/app/tab3/tab3.page.ts
+++ capstone/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  currentImage: any;
 
-  constructor() {}
+  constructor(private camera: Camera) {}
 
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log("Camera issue:" + err);
+    });
+  }
 }
