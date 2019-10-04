531c99b8d84b5b57d33d96cd2180ca7f0d4d6a80
David <michondavj@gmail.com>
2019-10-04 12:24:34 -0400

    Adding merged David and Jacob folders to development

---
+++ capstone/src/app/tab3/tab3.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+  currentImage: any;
+
+  constructor(private camera: Camera) {}
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log("Camera issue:" + err);
+    });
+  }
+}
