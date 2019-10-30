7fb10c78591da377d8a95db6ef0891f2abe7271f
Scott <41282567+sconeil@users.noreply.github.com>
2019-10-30 14:10:41 -0400

    firebase hosting
    added required files for firebase hosting, ran firebase init in /capstone directory

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
+var firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAU1WQY87ptzsZeD7IBwoVexggxOXcqEKg",
+  authDomain: "memcomb-8a195.firebaseapp.com",
+  databaseURL: "https://memcomb-8a195.firebaseio.com",
+  projectId: "memcomb-8a195",
+  storageBucket: "memcomb-8a195.appspot.com",
+  messagingSenderId: "1064540766139",
+  appId: "1:1064540766139:web:38b0e5ff72a4c1d5ab4e9e",
+  measurementId: "G-MQL5GSN06E"
+};
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
