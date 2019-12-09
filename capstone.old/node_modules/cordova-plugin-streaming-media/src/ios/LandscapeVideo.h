04639a5489d34760430c5c0c19f62645c56ee5e5
David Michon <dmichon@oakland.edu>
2019-12-08 21:35:22 -0500

    Deleting old capstone

--- capstone.old/node_modules/cordova-plugin-streaming-media/src/ios/LandscapeVideo.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <AVKit/AVKit.h>
-
-@interface LandscapeAVPlayerViewController: AVPlayerViewController
-{
-    
-}
-@end
--- capstone.old/node_modules/cordova-plugin-streaming-media/src/ios/PortraitVideo.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <AVKit/AVKit.h>
-
-@interface PortraitAVPlayerViewController: AVPlayerViewController
-{
-    
-}
-@end
